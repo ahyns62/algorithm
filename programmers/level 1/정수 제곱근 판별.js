c4fcdd627c650ce861970538d3aff62fdb8b448f
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2023-05-02 00:26:54 +0900

    Create 정수 제곱근 판별.js

---
+++ programmers/level 1/정수 제곱근 판별.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    const x = Math.sqrt(n);
+    
+    if(x % 1 === 0){
+        answer = Math.pow(x + 1, 2);
+    } else {
+        answer = -1;
+    }
+
+    return answer;
+}
