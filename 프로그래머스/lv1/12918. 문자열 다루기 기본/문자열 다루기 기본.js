7ec94c6107e71d70b3913633743cc3baf1649747
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2023-07-21 14:09:58 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer = true;
+    if(s.length === 4 || s.length === 6){
+        return s.split("").every(c => !isNaN(c))
+    }
+     else {
+         return false;
+     }
+}
