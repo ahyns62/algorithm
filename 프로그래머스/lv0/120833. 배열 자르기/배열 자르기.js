ce59d3a67846fde3cc7a3986569e480c0322dbc5
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2023-07-31 16:41:02 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,7 +1,3 @@
 function solution(numbers, num1, num2) {
-    let answer = [];
-    for(let i = num1; i <= num2; i++){
-        answer.push(numbers[i]);
-    }
-    return answer;
+    return numbers.slice(num1, num2 + 1);
 }
