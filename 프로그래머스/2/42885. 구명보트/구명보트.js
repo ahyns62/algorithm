d0c1699c6e29b5c1e185ca38a28bf5876904eba6
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-03-16 23:23:31 +0900

    [level 2] Title: 구명보트, Time: 33.56 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,18 @@
+function solution(people, limit) {
+    let ans = 0;
+    people.sort((a, b) => a - b);
+    let left = 0;
+    let right = people.length - 1;
+    
+    while(left <= right){
+        if(people[left] + people[right] <= limit){
+            left++;
+            right--;
+        } else {
+            right--;
+        }
+        ans++;
+    }
+    
+    return ans;
+}
