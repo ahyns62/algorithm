f48c5a43298ee80eecd0cd4038307416db613b48
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-03-26 21:35:02 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = [];
+    
+    for(let i = 2; i <= n; i++){
+        while(n % i === 0){
+            if(!answer.includes(i)){
+                answer.push(i);
+            }
+            n /= i;
+        }
+    }
+    
+    return answer;
+}
