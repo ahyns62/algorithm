b2b86687837788d54f78864e12547cadf3fea17f
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-08-15 23:21:10 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 26.76 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,11 +1,7 @@
 function solution(arr) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] >= 50 && arr[i] % 2 == 0){
-            arr[i] /= 2;
-        }
-        else if(arr[i] < 50 && arr[i] % 2 == 1){
-            arr[i] *= 2;
-        }
-    }
-    return arr;
+    return arr.map((num) => {
+        if(num >= 50 && num % 2 == 0) return num / 2;
+        else if(num < 50 && num % 2 == 1) return num * 2;
+        else return num;
+    });
 }
