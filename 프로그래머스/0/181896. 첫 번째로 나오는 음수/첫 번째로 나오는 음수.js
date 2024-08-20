b880d4fa7da8a821fb36e752a78a188e3f479bca
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-08-20 23:50:52 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,10 +1,3 @@
 function solution(num_list) {
-    let result = -1;
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] < 0){
-            result = i;
-            break;
-        }
-    }
-    return result;
+    return num_list.findIndex((v) => v < 0);
 }
