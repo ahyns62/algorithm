b8bbcc3506cd4af206452b36aea7628dc331c749
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-08-30 21:31:15 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 28.04 ms, Memory: 49.4 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,28 +1,32 @@
 function solution(enroll, referral, seller, amount) {
-    // parent 객체
+    // 판매원(자식) - 추천인(부모) 객체(parent)
     let parent = {};
     for(let i = 0; i < enroll.length; i++){
-        parent[enroll[i]] = referral[i]; 
-    };
-    
-    // result 객체
+        parent[enroll[i]] = referral[i]; // parent["john"] = "-"
+    }
+    // 판매원별 이익금 객체(result)
     let result = {};
-    for(let name of enroll){
-        result[name] = 0;
-    };
+    for(let i = 0; i < enroll.length; i++){
+        result[enroll[i]] = 0;
+    }
     
     // 이익 분배 계산
     seller.forEach((cur, i) => {
-      let money = amount[i] * 100;// 총 금액
-      let curSeller = cur; // 현재 사람 이름
-      // 판매자부터 차례대로 상위 노드로 이동하며 이익 분배
-      while(cur != "-" && money > 0){
-        const profit = Math.floor(money * 0.1); // 회사로 가는 돈
-        result[curSeller] += (money - profit); // 현재 노드에게 가는 돈
-        money = profit;
-        curSeller = parent[curSeller];
-      }  
-    });
-    
+        // 현재 판매하는 사람
+        let curName = cur;
+        // 이익금
+        let money = amount[i] * 100;
+        
+        // 상위 노드로 이익 전달 반복
+        while(cur != "-" && money > 0){
+            // 현재 판매하는 사람 자신한테 주는 돈 (90%)
+            result[curName] += money - Math.floor(money * 0.1);
+            // 현재 판매하는 사람 -> 상위 노드로 이동
+            curName = parent[curName];
+            // 상위 노드로 주는 돈 (10%)
+            money = Math.floor(money * 0.1);
+        }
+    })
     return enroll.map(name => result[name]);
+
 }
