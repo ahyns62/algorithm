165dbcf99a1a1e7bceb559b3b18a107197181a79
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-09-06 21:40:46 +0900

    [level 2] Title: 전화번호 목록, Time: 154.48 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,6 +1,8 @@
 function solution(phone_book) {
-    phone_book.sort(); // 이중 루프보다 정렬하는 것이 효율성 좋음
+    // 배열 정렬하기 (이중 for문 구현 시 효율성 테스트 실패)
+    phone_book.sort(); // 	[ '119', '1195524421', '97674223' ]
     for(let i = 0; i < phone_book.length - 1; i++){
+        // 다음 번호에서 현재 번호의 접두어가 있는 경우
         if(phone_book[i + 1].indexOf(phone_book[i]) === 0){
             return false;
         }
