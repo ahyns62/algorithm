7081c9265aaad1eb28933f3523d5a328e2a78c7f
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-09-06 19:36:16 +0900

    [level 2] Title: 전화번호 목록, Time: 130.65 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,9 @@
+function solution(phone_book) {
+    phone_book.sort(); // 이중 루프보다 정렬하는 것이 효율성 좋음
+    for(let i = 0; i < phone_book.length - 1; i++){
+        if(phone_book[i + 1].startsWith(phone_book[i])){
+            return false;
+        }
+    }
+    return true;
+}
