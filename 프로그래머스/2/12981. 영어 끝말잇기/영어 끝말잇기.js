3d770d9397f5be1ea3f4c69230547214e5efa7d2
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-09-06 21:17:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,12 +1,14 @@
 function solution(n, words) {
     let ans = {};
-    for (let i = 0; i < words.length; i++) {
-        let num = (i % n) + 1;  // 몇 번째 사람
+    for(let i = 0; i < words.length; i++){
+        let num = (i % n) + 1; // 몇 번째 사람
         let turn = Math.floor(i / n) + 1; // 몇 번째 차례
-        // 탈락 조건
+        // 탈락하는 경우
+        // 1. 이미 단어가 존재할 경우
+        // 2. 앞 사람 단어의 끝 글자 != 현재 단어의 첫글자
         if(ans[words[i]] || (i > 0 && words[i - 1].slice(-1) !== words[i][0])){
             return [num, turn];
-        } 
+        }
         ans[words[i]] = true; // 말한 단어 기록
     }
     return [0, 0];
