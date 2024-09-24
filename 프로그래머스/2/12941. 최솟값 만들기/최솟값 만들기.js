c63522ffceeaba991aef363c3990545aca433823
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-09-24 13:49:28 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.75 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,5 +2,5 @@ function solution(A,B){
     // 가장 작은 수(A) * 가장 큰 수(B)
     A.sort((a, b) => a - b); // 오름차순 1 2 4
     B.sort((a, b) => b - a); // 내림차순 5 5 4
-    return A.reduce((sum, curr, i) => sum += curr * B[i], 0);
+    return A.reduce((sum, curr, i) => sum + curr * B[i], 0);
 }
