9f7c7071ce610102d8402fb0a852b28e10a109d5
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2024-12-06 17:15:04 +0900

    [level 2] Title: 숫자의 표현, Time: 0.33 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,17 +1,9 @@
 function solution(n) {
-    let answer = 0;
-    for(let i = 1; i <= n; i++){
-        let sum = i;
-        let j = i + 1;
-        
-        while(sum < n){
-            sum += j;
-            j++;
-        }
-        
-        if(sum === n){
-            answer++;
-        }
-    }
-    return answer;
+  let answer = 0;
+  
+  for(let i = 0; i <= n; i++) {
+  	if(n % i === 0 && i % 2 === 1) answer++;
+  }
+  
+  return answer;
 }
