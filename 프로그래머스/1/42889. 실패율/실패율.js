0ecd1f720bbbe76eece6020dcc8b4724d0c9cc87
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-03-01 17:24:06 +0900

    [level 1] Title: 실패율, Time: 30.43 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,10 +1,27 @@
 function solution(N, stages) {
-    let ans = [];
-    for(let i = 1; i < N + 1; i++){
-        let num1 = stages.filter((x) => x === i).length;
-        let num2 = stages.filter((x) => x >= i).length;
-        ans.push([i, num1 / num2]);
+    let total = stages.length; 
+    
+    // 스테이지별 도전자 수
+    const challenger = new Array(N + 2).fill(0);
+    for(const stage of stages){
+        challenger[stage] += 1;
     }
-    ans.sort((a, b) => b[1] - a[1]);
-    return ans.map((x) => x[0]);
+    
+    // 스테이지별 실패한 사용자 수
+    const fails = {};
+    
+    // 실패율 계산
+    for(let i = 1; i <= N; i++){
+        if(challenger[i] === 0){
+            fails[i] = 0;
+            continue;
+        }
+        
+        fails[i] = challenger[i] / total;
+        total -= challenger[i];
+    }
+    
+    const result = Object.entries(fails).sort((a, b) => b[1] - a[1]); // 값 추출(실패율)
+    
+    return result.map((v) => Number(v[0])); // 스테이지 번호 추출
 }
