5598b77a9e6156547e2550f3525e28190a372a8d
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-03-05 21:21:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 33.57 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,16 @@
 function solution(number, k) {
-    const arr = [];
-    for (let i = 0; i < number.length; i++) {
-        // 배열 마지막 숫자와 현재 숫자를 비교
-        // 현재 숫자가 더 크면 해당 숫자를 pop함
-        while (number[i] > arr[arr.length - 1] && arr.length > 0 && k > 0) {
-            k--;
-            arr.pop();
-        }
-      arr.push(number[i]);
+  const stack = [];
+
+  for (const num of number) {
+    while (k > 0 && stack[stack.length - 1] < num) {
+      stack.pop();
+      k--;
     }
-    arr.splice(number.length - k, k);
-    return arr.join("");
+      
+    stack.push(num);
+  }
+
+  stack.splice(stack.length - k, k);
+
+  return stack.join("");
 }
