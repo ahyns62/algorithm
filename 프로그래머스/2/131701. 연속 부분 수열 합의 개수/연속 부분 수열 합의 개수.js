c7bbb8e0199b6dd94b9553c350e81f31304e5ece
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-06-07 23:20:23 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2761.66 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,21 +2,15 @@ function solution(elements) {
     const ans = new Set();
     const len = elements.length;
     
-    const getSum = (arr) => {
-        let sum = 0;
-        for(let i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        return sum;
-    }
-    
-    for(let i = 1; i <= len; i++){
-        for(let j = 0; j < len; j++){
-            if(j + i > len){
-                ans.add(getSum(elements.slice(j, len)) + getSum(elements.slice(0, j + i - len)));
-            } else{
-                ans.add(getSum(elements.slice(j, j + i)));
+    for(let size = 1; size <= len; size++){
+        for(let start = 0; start < len; start++){
+            let sum = 0;
+            
+            for(let k = 0; k < size; k++){
+                sum += elements[(start + k) % len];
             }
+            
+            ans.add(sum);
         }
     }
     
