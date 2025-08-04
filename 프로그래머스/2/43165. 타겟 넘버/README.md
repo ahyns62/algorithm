a11bcd920e9346e7a1da547ffbc5189fd280ad47
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-08-04 13:52:21 +0900

    [level 2] Title: 타겟 넘버, Time: 23.02 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 35.6 MB, 시간: 14.17 ms
+메모리: 35.5 MB, 시간: 23.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 15:22:58
+2025년 08월 04일 13:52:19
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,17 @@
-function solution(numbers, target) {    
-  let ans = 0;
+function solution(numbers, target) {
+    // 재귀 활용한 DFS
+    let answer = 0;
+    dfs(0, 0);
     
-  const dfs = (idx, sum) => {
-      if(idx === numbers.length){
-          if(sum === target) ans++;
-          return;
-      }
-  dfs(idx + 1, sum + numbers[idx]);
-  dfs(idx + 1, sum - numbers[idx]);
-};
-
-  dfs(0, 0);
-  return ans;
+    function dfs(index, sum){
+        if(index === numbers.length){
+            if(target === sum) answer++;
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    return answer;
 }
