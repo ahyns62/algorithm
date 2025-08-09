61da064ccaecc6c0dc3e45388e9ba40b004a05a7
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-08-09 14:58:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 40.10 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,30 +1,46 @@
 function solution(maps) {
-    const n = maps.length;
-    const m = maps[0].length;
-    
-    const dx = [0, 0, -1, 1];
+    // 최단 경로 -> BFS
+    const x = maps[0].length; // 행
+    const y = maps.length; // 열
+    const dx = [0, 0, -1, 1]; 
     const dy = [1, -1, 0, 0];
+    const visited = Array.from({length: y}, () => Array(x).fill(false));
     
-    const queue = [];
-    queue.push([0, 0]);
+    // 초기 실패 조건
+    if(maps[0][0] === 0 || maps[y - 1][x - 1] === 0){
+        return -1;
+    }
     
-     while (queue.length > 0) {
-        const [x, y] = queue.shift();
-
-        for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i];
-            const ny = y + dy[i];
-
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-
-            if (maps[nx][ny] === 0) continue;
-
-            if (maps[nx][ny] === 1) {
-                maps[nx][ny] = maps[x][y] + 1;
-                queue.push([nx, ny]);
+    // BFS
+    // 큐 삽입(y, x, 이동 칸 수) -> 방문 처리 -> 범위 체크, 벽 or 방문했으면 스킵
+    function bfs(yy, xx){
+        const queue = [];
+        queue.push([yy, xx, 1]);
+        visited[yy][xx] = true;
+        
+        while(queue.length){
+            const [cy, cx, dist] = queue.shift();
+            
+            if(cy === y - 1 && cx === x - 1){
+                return dist;
+            }
+            
+            for(let dir = 0; dir < 4; dir++){
+                const ny = cy + dy[dir];
+                const nx = cx + dx[dir];
+                
+                // 범위 체크
+                if (ny < 0 || ny >= y || nx < 0 || nx >= x) continue;
+                if (maps[ny][nx] === 0 || visited[ny][nx]) continue;
+                
+                visited[ny][nx] = true;
+                
+                queue.push([ny, nx, dist + 1]);
             }
         }
+        return -1;
     }
-
-    return maps[n - 1][m - 1] === 1 ? -1 : maps[n - 1][m - 1];
+    
+    
+    return bfs(0, 0);
 }
