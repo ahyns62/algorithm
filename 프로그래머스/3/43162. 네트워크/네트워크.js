bd4aa9c25cfb0e93fed3429a85bde227c44ce847
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-10-21 18:03:17 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,23 +1,27 @@
 function solution(n, computers) {
     let answer = 0;
-    const visited = Array(n).fill(false);
-    
-    function dfs(node){
-        visited[node] = true;
-        
-        for(let i = 0; i < n; i++){
-            if(computers[node][i] === 1 && !visited[i]){
-                dfs(i);
-            }
-        }
-    }
+    let visited = Array(n).fill(false);
     
     for(let i = 0; i < n; i++){
         if(!visited[i]){
             dfs(i);
             answer++;
         }
-    }
+    };
+    
+    function dfs(node){
+        visited[node] = true;
+        const computer = computers[node];
+        
+        for(let i = 0; i < computer.length; i++){
+            const isConnect = computer[i] === 1 ? true : false;
+            
+            if(isConnect && !visited[i]){
+                dfs(i);
+            }
+        }
+    };
+    
 
     return answer;
 }
