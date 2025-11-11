d3edb878ea3c6d5adb5b4d4e8e939b1aa992583e
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-11 23:37:07 +0900

    [level 3] Title: N으로 표현, Time: 19.97 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,24 +1,33 @@
 function solution(N, number) {
-
-    const dp = Array.from({length: 9}, () => new Set());
+    let answer = 0;
+    let s = [];
     
-    for(let i = 1; i < 9; i ++){
-        dp[i].add(Number(N.toString().repeat(i)));
-        
-        for(let j = 1; j < i; j++){
-            for(const a of dp[j]){
-                for(const b of dp[i - j]){
-                    dp[i].add(a+b);
-                    dp[i].add(a-b);
-                    dp[i].add(a*b);
-                    if (b !== 0) dp[i].add(Math.floor(a/b));
+    for (let i = 1; i < 9; i++){
+        s.push(new Set());
+        let e = "";
+        for (let j = 0; j<i; j++){
+            e += String(N)
+        }
+        s[i-1].add(parseInt(e));
+    }
+
+    for (let i = 0; i < 8; i++){
+        for (let j = 0; j < i; j++){
+            for (let e1 of s[j]){
+                   for (let e2 of s[i-j-1]){
+                           s[i].add(e1 + e2);
+                           s[i].add(e1 - e2);
+                           s[i].add(Math.floor(e1 / e2));
+                           s[i].add(e1 * e2);
                 }
             }
         }
-        
-        if(dp[i].has(number)) return i;
+        for (let k of s[i]){
+            if(k === number){
+                return i+1;
+            }
+        }
     }
-    
-    
+
     return -1;
 }
