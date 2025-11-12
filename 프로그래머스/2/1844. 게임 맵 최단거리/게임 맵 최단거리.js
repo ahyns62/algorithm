b0c9d04d8cba0ef07213d7dd1525741727cd1896
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-12 17:09:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 37.61 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,46 +1,37 @@
 function solution(maps) {
-    // 최단 경로 -> BFS
-    const x = maps[0].length; // 행
-    const y = maps.length; // 열
-    const dx = [0, 0, -1, 1]; 
-    const dy = [1, -1, 0, 0];
-    const visited = Array.from({length: y}, () => Array(x).fill(false));
+    // 방향 설정
+    const dx = [0, 0, -1, 1];
+    const dy = [-1, 1, 0, 0];
+    const n = maps.length;
+    const m = maps[0].length;
     
-    // 초기 실패 조건
-    if(maps[0][0] === 0 || maps[y - 1][x - 1] === 0){
-        return -1;
-    }
+    // 방문 배열 설정
+    let visited = Array.from({length: n}, () => Array(m).fill(false));
     
-    // BFS
-    // 큐 삽입(y, x, 이동 칸 수) -> 방문 처리 -> 범위 체크, 벽 or 방문했으면 스킵
-    function bfs(yy, xx){
-        const queue = [];
-        queue.push([yy, xx, 1]);
-        visited[yy][xx] = true;
+    // 시작점 설정
+    const queue = [[0, 0, 1]];
+    visited[0][0] = true;
+
+    // 탐색 함수(최단 경로 -> BFS)
+    while(queue.length){
+        const [x, y, dist] = queue.shift();
         
-        while(queue.length){
-            const [cy, cx, dist] = queue.shift();
+        if(x === n - 1 && y === m - 1) return dist;
+        
+        for(let i = 0; i < 4; i++){
+            const nx = x + dx[i];
+            const ny = y + dy[i];
             
-            if(cy === y - 1 && cx === x - 1){
-                return dist;
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || maps[nx][ny] === 0){
+            continue;
             }
             
-            for(let dir = 0; dir < 4; dir++){
-                const ny = cy + dy[dir];
-                const nx = cx + dx[dir];
-                
-                // 범위 체크
-                if (ny < 0 || ny >= y || nx < 0 || nx >= x) continue;
-                if (maps[ny][nx] === 0 || visited[ny][nx]) continue;
-                
-                visited[ny][nx] = true;
-                
-                queue.push([ny, nx, dist + 1]);
-            }
+            // 방문 처리 및 이동
+            visited[nx][ny] = true;
+            queue.push([nx, ny, dist + 1]);
         }
-        return -1;
     }
-    
-    
-    return bfs(0, 0);
+
+    return -1;
+
 }
