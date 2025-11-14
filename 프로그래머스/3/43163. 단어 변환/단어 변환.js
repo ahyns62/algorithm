2b2a8f2387344299bc59a0cab9daa8e0ec1c90f2
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-14 17:31:16 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,7 +1,7 @@
 // 최소 탐색 단계 구하기 -> BFS
 function solution(begin, target, words) {
     // 방문 객체 생성
-    let visited = new Set();
+    let visited = Array(words.length).fill(false);
     
     // 큐에 시작점 삽입 및 방문 처리
     let queue = [{word: begin, cnt: 0}];
@@ -14,10 +14,10 @@ function solution(begin, target, words) {
         }
         
         // 함수 조건 충족 시 탐색 & 방문 객체, 큐에 push
-        for(let nextWord of words){
-            if(canConvert(word, nextWord) && !visited.has(nextWord)){
-                visited.add(nextWord);
-                queue.push({word: nextWord, cnt: cnt+1});
+        for(let i = 0; i < words.length; i++){
+            if(canConvert(word, words[i]) && !visited[i]){
+                visited[i] = true;
+                queue.push({word: words[i], cnt: cnt+1});
             }
         }
     }
