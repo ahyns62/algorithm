eae09d85124972184b6132d682d95c87d1d2c10e
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-14 18:33:13 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.33 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,5 @@
 function solution(n) {
-    const dp = Array(n+1).fill(0);
-    
-    dp[0] = 1;
-    dp[1] = 1;
+    let dp = [1, 1];
     
     for (let i = 2; i <= n; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
