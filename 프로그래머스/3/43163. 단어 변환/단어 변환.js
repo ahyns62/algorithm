3f90ab41162973f8ba3a911257699448c450c808
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-14 17:16:28 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,36 +1,40 @@
+// 최소 탐색 단계 구하기 -> BFS
 function solution(begin, target, words) {
-    let queue = [];
+    // 방문 객체 생성
     let visited = new Set();
     
-    if(!words.includes(target)) return 0;
-    
-    queue.push({word: begin, steps: 0});
+    // 큐에 시작점 삽입 및 방문 처리
+    let queue = [{word: begin, cnt: 0}];
     
     while(queue.length > 0){
-        const { word, steps } = queue.shift();
+        const {word, cnt} = queue.shift();
         
         if(word === target){
-            return steps;
+            return cnt;
         }
         
-        for(let next of words){
-            if(!visited.has(next) && canConvert(word, next)){
-                visited.add(next);
-                queue.push({word: next, steps: steps + 1});
+        // 함수 조건 충족 시 탐색 & 방문 객체, 큐에 push
+        for(let nextWord of words){
+            if(canConvert(word, nextWord) && !visited.has(nextWord)){
+                visited.add(nextWord);
+                queue.push({word: nextWord, cnt: cnt+1});
             }
         }
     }
     
-    return 0;
-}
+    // 단어 변환 가능한 지 확인하는 함수
+    function canConvert(word1, word2){
+        let diffCount = 0;
+        for(let i = 0; i < word1.length; i++){
+            if(word1[i] !== word2[i]){
+                diffCount++;
+            }
 
-function canConvert(word1, word2){
-    let diff = 0;
-    for(let i = 0; i < word1.length; i++){
-        if(word1[i] !== word2[i]) diff++;
-        
-        if(diff > 1) return false;
+            if(diffCount > 1) return false;
+        }
+
+        return diffCount === 1;
     }
     
-    return diff === 1;
+    return 0;   
 }
