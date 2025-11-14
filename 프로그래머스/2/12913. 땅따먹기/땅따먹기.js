0ed21be6e15571bd906d103835a6d71ce3478a47
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-14 18:40:53 +0900

    [level 2] Title: 땅따먹기, Time: 66.16 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,19 +1,20 @@
-function solution(land) {
-    let answer = 0;
-    for(let i = 0; i < land.length; i++){
+// 누적 최대값 -> DP
+function solution(land) { 
+    let dp = Array.from({length: land.length}, () => Array(4).fill(0));
+    
+    for(let i = 0; i < 4; i++){
+        dp[0][i] = land[0][i];
+    }
+    
+    for(let i = 1; i < land.length; i++){
         for(let j = 0; j < 4; j++){
-            if(i === 0){
-                continue;
-            } else{
-                let arr = land[i-1].slice();
-                arr[j] = 0;
-                land[i][j] += Math.max.apply(null, arr);
-                
-                answer = Math.max(land[i][j], answer);
+            for(let k = 0; k < 4; k++){
+                if(j !== k){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + land[i][j]);
+                }
             }
         }
-        
     }
 
-    return answer;
+    return Math.max(...dp[land.length-1]);
 }
