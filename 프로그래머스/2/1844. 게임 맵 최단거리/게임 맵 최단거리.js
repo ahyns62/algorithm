479d6596aa1326fb43a90bcb1671950ba463bfbd
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-14 18:02:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 42.40 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,37 +1,42 @@
+// 칸의 최솟값 -> BFS
 function solution(maps) {
-    // 방향 설정
+    // 방향 생성
     const dx = [0, 0, -1, 1];
     const dy = [-1, 1, 0, 0];
-    const n = maps.length;
-    const m = maps[0].length;
+    const n = maps.length; // 5
+    const m = maps[0].length; // 5
     
-    // 방문 배열 설정
+    // 방문 배열 생성
     let visited = Array.from({length: n}, () => Array(m).fill(false));
     
-    // 시작점 설정
+    // 시작점 큐에 넣고 방문처리
     const queue = [[0, 0, 1]];
     visited[0][0] = true;
-
-    // 탐색 함수(최단 경로 -> BFS)
-    while(queue.length){
+    
+    // 큐가 빌 때까지 인접한 노드 넣고 방문 처리
+    while(queue.length > 0){
+        // 첫 요소 꺼내서 확인
         const [x, y, dist] = queue.shift();
         
+        // 1) 목표 도달 했을 경우
         if(x === n - 1 && y === m - 1) return dist;
         
+        // 2) 도달 안했을 경우
         for(let i = 0; i < 4; i++){
             const nx = x + dx[i];
             const ny = y + dy[i];
             
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || maps[nx][ny] === 0){
-            continue;
+            // 예외 조건 처리
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m || maps[nx][ny] === 0 || visited[nx][ny]){
+                continue;
             }
             
-            // 방문 처리 및 이동
-            visited[nx][ny] = true;
+            // 방문 처리
             queue.push([nx, ny, dist + 1]);
+            visited[nx][ny] = true;
         }
     }
 
+    
     return -1;
-
 }
