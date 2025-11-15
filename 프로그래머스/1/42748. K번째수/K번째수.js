d9c86731180490ace0fb31b3d9a870df2603ac32
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-15 19:08:50 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -2,7 +2,8 @@ function solution(array, commands) {
     let answer = [];
     for(let i = 0; i < commands.length; i++){
         let [a, b, c] = commands[i];
-        answer.push(array.slice(a - 1, b).sort((a, b) => a - b)[c - 1]); 
+        answer.push(array.slice(a-1, b).sort((a, b) => a - b)[c-1]);
     }
+    
     return answer;
 }
