0bd573dae3bed74ab031e4b36215f9406f73f36e
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-15 15:48:27 +0900

    [level 3] Title: 정수 삼각형, Time: 8.01 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,12 +1,17 @@
+
 function solution(triangle) {
-    // bottom-up 방식의 DP
-    const height = triangle.length;
+    const n = triangle.length;
+    let dp = Array.from({length: n}, () => Array(n).fill(0));
+    dp[0][0] = triangle[0][0];
     
-    for(let i = height - 2; i >= 0; i--){
-        for(let j = 0; j < triangle[i].length; j++){
-            triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    for(let i = 1; i < n; i++){
+        for(let j = 0; j <= i; j++){
+            let fromLeft = (j > 0) ? dp[i-1][j-1] : 0;
+            let fromRight = (j < i) ? dp[i-1][j] : 0;
+            
+            dp[i][j] = Math.max(fromLeft, fromRight) + triangle[i][j];
         }
     }
     
-    return triangle[0][0];
+    return Math.max(...dp[n-1]);
 }
