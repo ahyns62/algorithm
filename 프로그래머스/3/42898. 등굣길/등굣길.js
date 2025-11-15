7a86461225db53315b5c634049026658ea4bc28f
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-15 16:44:31 +0900

    [level 3] Title: 등굣길, Time: 1.32 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,29 +1,27 @@
+// 최적의 최단 경로 개수 -> DP
 function solution(m, n, puddles) {
-    const MOD = 1_000_000_007;
-    // dp[y][x] = (1,1)에서 (x,y)까지의 경로 수
-    let dp = Array.from({length: n + 1}, () => Array(m + 1).fill(0));
-    let isPuddles = Array.from({length: n + 1}, () => Array(m + 1).fill(false));
+    let dp = Array.from({length: n}, () => Array(m).fill(0));
     
-    // 물 웅덩이 표시
-    for(const [x, y] of puddles){
-        isPuddles[y][x] = true;
+    for(let [x, y] of puddles){
+        dp[y-1][x-1] = -1;
     }
     
-    // dp 테이블
-    dp[1][1] = 1;
-    
-    for(let i = 1; i <= n; i++){
-        for(let j = 1; j <= m; j++){
-            if(j === 1 && i === 1) continue;
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            dp[0][0] = 1;
             
-            if(isPuddles[i][j]){
+            if(dp[i][j] === -1){
                 dp[i][j] = 0;
-            } else {
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+                continue;
             }
+            
+            let fromTop = (i > 0 && dp[i-1][j] !== -1) ? dp[i-1][j] : 0;
+            let fromLeft = (j > 0 && dp[i][j-1] !== -1) ? dp[i][j-1] : 0;
+            
+            dp[i][j] = (fromTop + fromLeft) % 1000000007;
+            
         }
     }
     
-    
-    return dp[n][m];
+    return dp[n-1][m-1];
 }
