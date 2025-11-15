d2b882ef7020586fdd4efa53d2b9c937ce7dd5c0
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-15 17:57:08 +0900

    [level 4] Title: 도둑질, Time: 35.72 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.js
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -1,16 +1,19 @@
 function solution(money) {
     const n = money.length;
+    if(n === 1) return money[0];
+    if(n === 2) return Math.max(money[0], money[1]);
     
-    // 첫 번째 집을 털 경우
-    const dp1 = Array(n).fill(0);
+    // 1. 첫번째 집 털 경우
+    let dp1 = Array(n).fill(0);
     dp1[0] = money[0];
     dp1[1] = Math.max(money[0], money[1]);
-    for(let i = 2; i < n - 1; i++){
+    
+    for(let i = 2; i < n-1; i++){
         dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);
     }
     
-    // 마지막 집을 털 경우
-    const dp2 = Array(n).fill(0);
+    // 2. 두번째 집 털 경우
+    let dp2 = Array(n).fill(0);
     dp2[0] = 0;
     dp2[1] = money[1];
     for(let i = 2; i < n; i++){
