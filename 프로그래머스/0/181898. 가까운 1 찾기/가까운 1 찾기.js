9c2dd5de018207b8950f42a3cb00a436fd5b0470
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-11-24 23:40:27 +0900

    [level 0] Title: 가까운 1 찾기, Time: 26.18 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,11 @@
+function solution(arr, idx) {
+    for(let i = 0; i < arr.length; i++){
+        if(i >= idx){
+            if(arr[i] === 1){
+                return i;
+            }
+        }
+        
+    }
+    return -1;
+}
